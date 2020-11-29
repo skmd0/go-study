e19de99be8fff145a72deececefc2092478d2cd5
Domen Skamlic <domen@skamlic.com>
2020-11-29 18:07:53 +0100

    add slice Stdin args parse and sort exercise

---
+++ exercises/slices/10-append-sort-nums/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Append and Sort Numbers
+//
+//  We'll have a []string that should contain numbers.
+//
+//  Your task is to convert the []string to an int slice.
+//
+//  1. Get the numbers from the command-line
+//
+//  2. Append them to an []int
+//
+//  3. Sort the numbers
+//
+//  4. Print them
+//
+//  5. Handle the error cases
+//
+//
+// EXPECTED OUTPUT
+//
+//  go run main.go
+//    provide a few numbers
+//
+//  go run main.go 4 6 1 3 0 9 2
+//    [0 1 2 3 4 6 9]
+//
+//  go run main.go a b c
+//    []
+//
+//  go run main.go 4 a 1 c d 9
+//    [1 4 9]
+//
+// ---------------------------------------------------------
+
+func main() {
+	numArgs := os.Args[1:]
+	if len(numArgs) == 0 {
+		fmt.Println("Please provide numbers with arguments.")
+		return
+	}
+
+	nums := make([]int, 0, len(numArgs))
+	for _, na := range numArgs {
+		n, err := strconv.Atoi(na)
+		if err != nil {
+			fmt.Printf("Argument '%s' is in invalid format.\n", na)
+			continue
+		}
+		nums = append(nums, n)
+	}
+	sort.Ints(nums)
+	fmt.Println(nums)
+}
