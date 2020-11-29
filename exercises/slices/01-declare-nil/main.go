197cd36690e97e991ee826ef38bccc0356acb199
Domen Skamlic <domen@skamlic.com>
2020-11-29 17:51:35 +0100

    add basic slice nil declaration exercise

---
+++ exercises/slices/01-declare-nil/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Declare nil slices
+//
+//  1. Declare the following slices as nil slices:
+//
+//    1. The names of your friends (names slice)
+//
+//    2. The distances to locations (distances slice)
+//
+//    3. A data buffer (data slice)
+//
+//    4. Currency exchange ratios (ratios slice)
+//
+//    5. Up/Down status of web servers (alives slice)
+//
+//
+//  2. Print their type, length and whether they're equal to nil value or not.
+//
+//
+// EXPECTED OUTPUT
+//  names    : []string 0 true
+//  distances: []int 0 true
+//  data     : []uint8 0 true
+//  ratios   : []float64 0 true
+//  alives   : []bool 0 true
+// ---------------------------------------------------------
+
+func main() {
+	var (
+		names     []string
+		distances []int
+		data      []uint8
+		ratios    []float64
+		alives    []bool
+	)
+
+	fmt.Printf("%T %d, %t\n", names, len(names), names == nil)
+	fmt.Printf("%T %d, %t\n", distances, len(distances), distances == nil)
+	fmt.Printf("%T %d, %t\n", data, len(data), data == nil)
+	fmt.Printf("%T %d, %t\n", ratios, len(ratios), ratios == nil)
+	fmt.Printf("%T %d, %t\n", alives, len(alives), alives == nil)
+}
