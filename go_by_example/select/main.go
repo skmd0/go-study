c27ddbcd6582e677f77f32e5a851d53c49f4acb8
Domen Skamlic <domen@skamlic.com>
2021-03-02 20:10:42 +0100

    add channel select pattern example

---
+++ go_by_example/select/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	// create a select channel pattern and print each result
+	// solution: https://gobyexample.com/select
+}
