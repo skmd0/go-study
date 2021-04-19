6b1cdf05c5a6ba53237bbe53fcb222a52dc4e465
Domen Skamlic <domen@skamlic.com>
2021-04-19 11:57:23 +0200

    add empty parameter default case

--- learn_go_by_tests/hello_world/hello_test.go
+++ learn_go_by_tests/hello_world/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Domen")
-	want := "Hello, Domen"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// t.Helper helps report the correct line when test fails
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Domen")
+		want := "Hello, Domen"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' using empty parameter", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
