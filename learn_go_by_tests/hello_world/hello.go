dd0a370dc9004146636fe6bf7870a1bb71aa7b15
Domen Skamlic <domen@skamlic.com>
2021-04-19 12:15:10 +0200

    add support for languages

--- learn_go_by_tests/hello_world/hello.go
+++ learn_go_by_tests/hello_world/hello.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
+const (
+	englishHelloPrefix   = "Hello, "
+	spanishHelloPrefix   = "Hola, "
+	spanishLang          = "ES"
+	frenchHelloPrefix    = "Bonjour, "
+	frenchLang           = "FR"
+	slovenianHelloPrefix = "Zdravo, "
+	slovenianLang        = "SI"
+)
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return helloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanishLang:
+		return spanishHelloPrefix
+	case frenchLang:
+		return frenchHelloPrefix
+	case slovenianLang:
+		return slovenianHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
 }
 
 func main() {
-	fmt.Println(Hello("Domen"))
+	fmt.Println(Hello("Domen", ""))
 }
--- learn_go_by_tests/hello_world/hello_test.go
+++ learn_go_by_tests/hello_world/hello_test.go
@@ -11,13 +11,28 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Domen")
+		got := Hello("Domen", "")
 		want := "Hello, Domen"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' using empty parameter", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Domen", "ES")
+		want := "Hola, Domen"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Domen", "FR")
+		want := "Bonjour, Domen"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Slovenian", func(t *testing.T) {
+		got := Hello("Domen", "SI")
+		want := "Zdravo, Domen"
 		assertCorrectMessage(t, got, want)
 	})
 }
